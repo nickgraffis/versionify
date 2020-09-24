519d6b1d14216c4a43f7df084647ad6bf8a35cc2
Nick Graffis <nicholasgraffis@gmail.com>
2020-09-24 12:12:36 -0700

    Added optional script parameter "placement".

--- versionify.js
+++ versionify.js
@@ -1,10 +1,14 @@
-function scripts (file, type = 'text/javascript') {
+function scripts (file, placement = 'head', type = 'text/javascript') {
     var script = document.createElement('script');
     script.src = file + '?v=' + Math.random();
     script.type = type;
     script.defer = true;
-
-    document.getElementsByTagName('head').item(0).appendChild(script);
+    
+    if (placement === 'head') {
+        document.getElementsByTagName('head').item(0).appendChild(script);
+    } esle if (placement === 'body') {
+        document.getElementsByTagName('body').item(0).appendChild(script);
+    }
 }
 
 function styles (file) {
